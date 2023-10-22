eb969a0682109c304750fbf5daacd9ccc7ef09e1
iamduong123 <93998682+iamduong123@users.noreply.github.com>
2023-10-22 21:19:38 +0700

    Add files via upload

--- Word.java
+++ Word.java
@@ -13,6 +13,15 @@ public class Word {
 
     public static final Set<String> stopWords = new HashSet<>();
 
+// Somewhere in the code (maybe during initialization or in a static block), you should add "of" to the set:
+
+    static {
+        stopWords.add("of");
+        stopWords.add("the");
+        // add other stop words as necessary
+    }
+
+
     private final String prefix;
     private final String text;
     private final String suffix;
@@ -22,16 +31,17 @@ public class Word {
         this.text = text;
         this.suffix = suffix;
     }
-
+    public static boolean isStopWord(String word) {
+        return stopWords.contains(word.toLowerCase());
+    }
     public boolean isKeyword() {
+        // Check if the word matches the desired pattern and is not a stop word.
         if (!Pattern.compile("[a-zA-Z'-]+").matcher(text).matches() || text.isEmpty()) {
             return false;
         }
 
         return !stopWords.contains(text.toLowerCase());
     }
-
-
 
     public String getPrefix() {
         return prefix;
@@ -59,23 +69,37 @@ public class Word {
     }
 
     public static Word createWord(String rawText) {
-        int startIdx = 0, endIdx = 0;
-
-        while (startIdx < rawText.length() && !Character.isLetter(rawText.charAt(startIdx))) {
-            startIdx++;
+        int textStart = 0;
+        while (textStart < rawText.length() && !Character.isLetterOrDigit(rawText.charAt(textStart))) {
+            textStart++;
         }
 
-        endIdx = startIdx;
-        while (endIdx < rawText.length() && (Character.isLetter(rawText.charAt(endIdx)) ||
-                rawText.charAt(endIdx) == '-' || rawText.charAt(endIdx) == '\'')) {
-            endIdx++;
+        int textEnd = textStart;
+        while (textEnd < rawText.length() && (Character.isLetterOrDigit(rawText.charAt(textEnd)) || rawText.charAt(textEnd) == '-' || rawText.charAt(textEnd) == '\'')) {
+            textEnd++;
         }
 
-        String prefix = rawText.substring(0, startIdx);
-        String text = rawText.substring(startIdx, endIdx);
-        String suffix = rawText.substring(endIdx);
+        String prefixWord = rawText.substring(0, textStart);
+        String textWord = rawText.substring(textStart, textEnd);
+        String suffixWord = rawText.substring(textEnd);
 
-        return new Word(prefix, text, suffix);
+        if (textWord.endsWith("'s")) {
+            suffixWord = "'s" + suffixWord;
+            textWord = textWord.substring(0, textWord.length() - 2);
+        }
+
+        // Use the validWord function to check validity
+        if (!validWord(textWord)) {
+            prefixWord = "";
+            textWord = rawText;
+            suffixWord = "";
+        }
+
+        return new Word(prefixWord, textWord, suffixWord);
+    }
+
+    private static boolean validWord(String word) {
+        return word.matches("^[a-zA-Z'-]+$") && !word.isEmpty();
     }
 
     public static boolean loadStopWords(String fileName) {
