a58ff9f26d8da30348ac3c8801b3c87b836ec9db
iamduong123 <93998682+iamduong123@users.noreply.github.com>
2023-10-22 21:35:50 +0700

    Add files via upload

--- Query.java
+++ Query.java
@@ -6,11 +6,10 @@ public class Query {
     private List<Word> keywords = new ArrayList<>();
 
     public Query(String searchPhrase) {
-        searchPhrase = searchPhrase.replaceAll("[^a-zA-Z0-9\\s]", "");
         Arrays.stream(searchPhrase.split("\\s+"))
                 .map(Word::createWord)
+                .filter(word -> !Word.isStopWord(word.getText()))  // filter out stop words
                 .filter(Word::isKeyword)
-                .distinct()
                 .forEach(keywords::add);
     }
 
@@ -20,6 +19,7 @@ public class Query {
 
     public List<Match> matchAgainst(Doc document) {
         List<Match> matches = new ArrayList<>();
+
         List<Word> allWords = new ArrayList<>();
         allWords.addAll(document.getTitle());
         allWords.addAll(document.getBody());
@@ -29,7 +29,7 @@ public class Query {
             int firstOccurrence = -1;
 
             for (int i = 0; i < allWords.size(); i++) {
-                if (keyword.equals(allWords.get(i))) {
+                if (keyword.getText().equalsIgnoreCase(allWords.get(i).getText())) {
                     frequency++;
                     if (firstOccurrence == -1) {
                         firstOccurrence = i;
@@ -42,64 +42,10 @@ public class Query {
             }
         }
 
-        Collections.sort(matches);
+        // Sort matches by firstOccurrence
+        matches.sort(Comparator.comparingInt(Match::getFirstIndex));
+
         return matches;
     }
 
-
-
-    private int countFrequencyInList(List<Word> words, Word keyword) {
-        int frequency = 0;
-        for (Word word : words) {
-            if (keyword.equals(word)) {
-                frequency++;
-            }
-        }
-        return frequency;
-    }
-
-    private int findInitialPositionInList(List<Word> words, Word keyword) {
-        for (int i = 0; i < words.size(); i++) {
-            if (keyword.equals(words.get(i))) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-
-
-    private int countFrequency(Doc document, Word keyword) {
-        int frequency = 0;
-
-        for (Word titleWord : document.getTitle()) {
-            if (keyword.equals(titleWord)) {
-                frequency++;
-            }
-        }
-
-        for (Word bodyWord : document.getBody()) {
-            if (keyword.equals(bodyWord)) {
-                frequency++;
-            }
-        }
-
-        return frequency;
-    }
-
-    private int findInitialPosition(Doc document, Word keyword) {
-        for (Word titleWord : document.getTitle()) {
-            if (keyword.equals(titleWord)) {
-                return document.getTitle().indexOf(keyword);
-            }
-        }
-
-        for (Word bodyWord : document.getBody()) {
-            if (keyword.equals(bodyWord)) {
-                return document.getTitle().size() + document.getBody().indexOf(keyword);
-            }
-        }
-
-        return -1;
-    }
 }
